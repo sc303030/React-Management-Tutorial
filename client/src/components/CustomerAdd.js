c440ea8eaa720941e3c97ccf9f3431025087db2b
GyeongHyeon <lkh6035@naver.com>
2021-07-02 05:13:29 +0900

    21.07.02 | 12강_7m

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -95,36 +95,45 @@ class CustomerAdd extends React.Component {
     }
 
     render() {
-        const { classes } = this.props;
+        // const { classes } = this.props;
         return (
-            <div>
-                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
-                    고객 추가하기
-                </Button>
-                <Dialog open={this.state.open} onClose={this.handleClose}>
-                    <DialogTitle>고객 추가</DialogTitle>
-                    <DialogContent>
-                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                        <label htmlFor="raised-button-file">
-                            <Button variant="contained" color="primary" component="span" name="file">
-                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
-                            </Button>
-                        </label>
-                        <br/>
-                        <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
-                        <TextField label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                        <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                        <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
-                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
-                    </DialogActions>
-                </Dialog>
-            </div>
+            <form onSubmit={this.handleFormSubmit}>
+                <h1>고객 추가</h1>
+                프로필 이미지 : <input type="file" name="file" file={this.state.file} value={this.state.fileNmae} onChange={this.handleFileChange} /><br/>
+                이름 : <input type="text" name="username" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+                생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChanged}/><br/>
+                직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChanged}/><br/>
+                <buttom type="submit">추가하기</buttom>
+            </form>
+            // <div>
+            //     <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+            //         고객 추가하기
+            //     </Button>
+            //     <Dialog open={this.state.open} onClose={this.handleClose}>
+            //         <DialogTitle>고객 추가</DialogTitle>
+            //         <DialogContent>
+            //             <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+            //             <label htmlFor="raised-button-file">
+            //                 <Button variant="contained" color="primary" component="span" name="file">
+            //                     {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+            //                 </Button>
+            //             </label>
+            //             <br/>
+            //             <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+            //             <TextField label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+            //             <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+            //             <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+            //         </DialogContent>
+            //         <DialogActions>
+            //             <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+            //             <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+            //         </DialogActions>
+            //     </Dialog>
+            // </div>
         )
     }
 
 }
-
-export default withStyles(styles)(CustomerAdd);
+export default CustomerAdd;
+// export default withStyles(styles)(CustomerAdd);
